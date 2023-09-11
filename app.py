ada496fca961ca9a3ccabfec0f1e4983c02cf6fa
Malvina Guex <malvina.guex@gmail.com>
2023-09-11 12:22:35 +0200

    theme added + basic FE. API call not working

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
-#Malvina having fun with git and loving it!
-
+import streamlit as st
+import requests
+from PIL import Image
+from io import BytesIO
 
 # Set page title and favicon
 st.set_page_config(
@@ -17,11 +19,36 @@ st.subheader("Is your image real?")
 url = 'https://deepfakepreproc-abexurulqa-ew.a.run.app/docs'
 
 # File upload widget
-uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+uploaded_image = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
-# Display uploaded image if available
-if uploaded_file is not None:
-    st.image(uploaded_file, caption="Uploaded Image", use_column_width=True)
+# Check if an image is uploaded
+if uploaded_image:
+    st.image(uploaded_image, caption="Uploaded Image", use_column_width=True)
+
+    # Make an API request when a button is clicked
+    if st.button("Get Prediction"):
+        # API endpoint URL
+        api_url = "https://deepfakepreproc-abexurulqa-ew.a.run.app/upload_image"  # Replace with your API URL
+
+        try:
+            # Prepare the image for API request
+            image_data = BytesIO(uploaded_image.read())
+            files = {"image": ("image.jpg", image_data)}
+
+            # Send the image to the API as a POST request
+            response = requests.post(api_url, data=files)
+
+            if response.status_code == 200:
+                # Parse the API response (assuming it's JSON)
+                prediction = response.json()
+                st.write("Prediction:", prediction["label"])
+            else:
+                st.write("Error:", response.status_code, response.text)
+
+        except Exception as e:
+            st.write("An error occurred:", str(e))
+
+
 
 # Prediction result section
 st.markdown("### Prediction Result")
